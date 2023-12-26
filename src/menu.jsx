66fa8ad437173fcd54fe77689bb73db22722066f
42Zach <zachajohnson2004@gmail.com>
2023-12-26 12:31:56 -0700

    Added Change

--- src/menu.jsx
+++ src/menu.jsx
@@ -1,12 +1,22 @@
 import './menu.css'
 import './NavBar.css'
 import OtherNavbar from './otherNavbar.jsx'
+import SpecialsPicture from './coffee-magazine-post.jpg'
 
 function Menu() {
     return (
         <>
             <OtherNavbar />
+            <SpecialsBG />
         </>
+    )
+}
+
+function SpecialsBG() {
+    return (
+        <div className="SpecialsBG">
+            <img src={SpecialsPicture} alt='' className='SpecialsPicture' />
+        </div>
     )
 }
 
