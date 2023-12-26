0afe373140bd377ec6c84fb9c1200eafc49c0e20
42Zach <zachajohnson2004@gmail.com>
2023-12-26 14:59:17 -0700

    Added Change

--- src/menu.jsx
+++ src/menu.jsx
@@ -1,7 +1,7 @@
 import './menu.css'
 import './NavBar.css'
 import OtherNavbar from './otherNavbar.jsx'
-import SpecialsPicture from './coffee-magazine-post.jpg'
+import SpecialsPicture from './SpecialsImage.jpg'
 
 function Menu() {
     return (
@@ -14,9 +14,16 @@ function Menu() {
 
 function SpecialsBG() {
     return (
-        <div className="SpecialsBG">
-            <img src={SpecialsPicture} alt='' className='SpecialsPicture' />
-        </div>
+        <>
+            <div className="SpecialsBG">
+                <img src={SpecialsPicture} alt='' className='SpecialsPicture' />
+                <div className="SpecialsLogo"><em>The Art Of Coffee Elevated</em></div>
+            </div>
+            <div className="SpecialsTitle-1">
+                <span className='SpecialsTitle-2'>Specials</span>
+            </div>
+        </>
+        
     )
 }
 
