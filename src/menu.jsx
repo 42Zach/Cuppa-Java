2100f9633589a9f245c3ea4f4a8c59012818abc2
42Zach <zachajohnson2004@gmail.com>
2024-01-05 16:26:23 -0700

    Added Change

--- src/menu.jsx
+++ src/menu.jsx
@@ -21,11 +21,68 @@ function SpecialsBG() {
             <div className="SpecialsTitle-1">
                 <span className='SpecialsTitle-2'>Specials</span>
             </div>
+
+            <div className="MainContainer">
+                <div className="container shoe">
+                    <div className="productImage shoeImg"></div>
+                    <div className="size shoeSize">
+                        <h4>SIZE</h4>
+                        <ul>
+                            <li>S</li>
+                            <li>M</li>
+                            <li>L</li>
+                        </ul>
+                    </div>
+                    <div className="price shoePrice">
+                        <h4>PRICE</h4>
+                        <span>$6</span>
+                    </div>
+                    <div className="color shoeColor">
+                        <h4>Notes</h4>
+                        <ul>
+                            <li><span>Vanilla</span></li>
+                            <li><span>Maple</span></li>
+                            <li><span></span></li>
+                        </ul>
+                    </div>
+                    <div className="productName shoeName">Americano</div>
+                </div>
+
+
+                
+            </div>
+
         </>
-        
     )
 }
 
-/* Coffee is full of Joy, Love, Peace, Laughter */
+/*
+
+                <div className="container shirt">
+                    <div className="productImage shirtImg"></div>
+                    <div className="size shirtSize">
+                        <h4>SIZE</h4>
+                        <ul>
+                            <li>S</li>
+                            <li>M</li>
+                            <li>L</li>
+                        </ul>
+                    </div>
+                    <div className="price shirtPrice">
+                        <h4>PRICE</h4>
+                        <span>$50</span>
+                    </div>
+                    <div className="color shirtColor">
+                        <h4>Notes</h4>
+                        <ul>
+                            <li><span>Vanilla</span></li>
+                            <li><span></span></li>
+                            <li><span></span></li>
+                        </ul>
+                    </div>
+                    <div className="productName shirtName">Crew-neck t-shirt</div>
+                </div>
+
+*/
 
 export default Menu
