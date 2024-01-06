162fd42dcf02b0f598267f5f84194299f502e23d
42Zach <zachajohnson2004@gmail.com>
2024-01-06 16:20:09 -0700

    Added cart icon to home nav and standard nav

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -91,7 +91,11 @@ class HoverButton {
             <button className='FunBtn F2'><Link to='/menu' className='F2'>Menu</Link></button>
           </li>
           <li className='Funli'>
-            <button className='FunBtn F3'><Link to='/cart' className='F3'>Cart</Link></button>
+            <button className='FunBtn F3'><Link to='/cart' className='F3'><svg className='Cart' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart CartImg" viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L
+                        2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 
+                        0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" stroke='currentColor' stroke-width='.5'/>
+                    </svg></Link></button>
           </li>
         </ul>
       </div>
